65793aa3a820df78103d8e4a17304caab79735f2
Shijen.nelliulathil <shijen.nelliulathil@happiestminds.com>
2024-08-21 14:33:41 +0530

    Removed explicit jokes

--- app/src/main/java/com/shijen/a4o/data/remote/JokesApi.kt
+++ app/src/main/java/com/shijen/a4o/data/remote/JokesApi.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface JokesApi {
-    @GET("joke/Any")
+    @GET("joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit")
     suspend fun getJoke(): Response<JokeResp>
 }
